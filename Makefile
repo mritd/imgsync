69d61a83e95813402558b91332f51fe72150ae7b
mritd <mritd@linux.com>
2020-04-17 17:36:17 +0800

    feat(version): add version flag
    add version flag
    
    Signed-off-by: mritd <mritd@linux.com>

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
-BUILD_VERSION   := $(version)
+BUILD_VERSION   := $(shell cat version)
 BUILD_TIME      := $(shell date "+%F %T")
 COMMIT_SHA1     := $(shell git rev-parse HEAD)
 
 all:
 	gox -osarch="darwin/amd64 linux/386 linux/amd64" \
         -output="dist/{{.Dir}}_{{.OS}}_{{.Arch}}" \
-        -tags="containers_image_openpgp"
+        -tags="containers_image_openpgp" \
+        -ldflags   "-X 'github.com/mritd/imgsync/cmd.version=${BUILD_VERSION}' \
+					-X 'github.com/mritd/imgsync/cmd.buildTime=${BUILD_TIME}' \
+					-X 'github.com/mritd/imgsync/cmd.commit=${COMMIT_SHA1}'"
 
 release: all
 	ghr -u mritd -t ${GITHUB_RELEASE_TOKEN} -replace -recreate --debug ${BUILD_VERSION} dist
@@ -14,7 +17,10 @@ clean:
 	rm -rf dist
 
 install:
-	go install -tags="containers_image_openpgp"
+	go install -tags="containers_image_openpgp" \
+			   -ldflags "-X 'github.com/mritd/imgsync/cmd.version=${BUILD_VERSION}' \
+						 -X 'github.com/mritd/imgsync/cmd.buildTime=${BUILD_TIME}' \
+						 -X 'github.com/mritd/imgsync/cmd.commit=${COMMIT_SHA1}'"
 
 .PHONY: all release clean install
 
--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,23 @@
 package cmd
 
 import (
+	"encoding/base64"
+	"fmt"
+	"runtime"
+
 	"github.com/sirupsen/logrus"
 
 	"github.com/spf13/cobra"
 )
 
+var version, buildTime, commit string
+
 var debug bool
 
 var rootCmd = &cobra.Command{
-	Use:   "imgsync",
-	Short: "Docker image sync tool",
+	Use:     "imgsync",
+	Short:   "Docker image sync tool",
+	Version: version,
 	Long: `
 Docker image sync tool.`,
 	Run: func(cmd *cobra.Command, args []string) {
@@ -27,6 +34,7 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initLog)
 	rootCmd.PersistentFlags().BoolVar(&debug, "debug", false, "debug mode")
+	rootCmd.SetVersionTemplate(versionTpl())
 }
 
 func initLog() {
@@ -38,4 +46,18 @@ func initLog() {
 	if debug {
 		logrus.SetLevel(logrus.DebugLevel)
 	}
+}
+
+func versionTpl() string {
+	var bannerBase64 = "ZSAgZWVlZWVlZSBlZWVlZSBlZWVlZSBlICAgIGUgZWVlZWUgZWVlZQo4ICA4ICA4ICA4IDggICA4IDggICAiIDggICAgOCA4ICAgOCA4ICA4CjhlIDhlIDggIDggOGUgICAgOGVlZWUgOGVlZWU4IDhlICA4IDhlCjg4IDg4IDggIDggODggIjggICAgODggICA4OCAgIDg4ICA4IDg4Cjg4IDg4IDggIDggODhlZTggOGVlODggICA4OCAgIDg4ICA4IDg4ZTgK"
+	var tpl = `%s
+Name: imgsync
+Version: %s
+Arch: %s
+BuildTime: %s
+CommitID: %s
+`
+
+	banner, _ := base64.StdEncoding.DecodeString(bannerBase64)
+	return fmt.Sprintf(tpl, banner, version, runtime.GOOS+"/"+runtime.GOARCH, buildTime, commit)
 }
