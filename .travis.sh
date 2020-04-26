6ebca1ac1a506e851396768d7d8a66e019513f77
mritd <mritd@linux.com>
2020-04-26 23:10:16 +0800

    chore(ci): fix sh
    fix sh
    
    Signed-off-by: mritd <mritd@linux.com>

--- .travis.sh
+++ .travis.sh
@@ -4,7 +4,7 @@ set -e
 
 export TZ=UTC-8
 cd ${HOME}/manifests
-if [ ! git diff-index --quiet HEAD -- ]; then
+if [ ! git diff-index --quiet HEAD ]; then
   git add .
   git commit -m "Travis CI Auto Update(`date +'%Y-%m-%d %H:%M:%S'`)"
   git push https://mritd:${GITHUB_TOKEN}@github.com/mritd/gcr.git
