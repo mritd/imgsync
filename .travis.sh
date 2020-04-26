2ea065e3ce8364d0eb401dc94757d3da534c1d0e
mritd <mritd@linux.com>
2020-04-26 23:29:52 +0800

    chore(ci): fix push sh
    fix push sh
    
    Signed-off-by: mritd <mritd@linux.com>

--- .travis.sh
+++ .travis.sh
@@ -4,7 +4,7 @@ set -e
 
 export TZ=UTC-8
 cd ${HOME}/manifests
-if [ -z "$(git status --porcelain)" ]; then
+if [ -n "$(git status --porcelain)" ]; then
   git add .
   git commit -m "Travis CI Auto Update(`date +'%Y-%m-%d %H:%M:%S'`)"
   git push https://mritd:${GITHUB_TOKEN}@github.com/mritd/gcr.git
