43fd49518aa47dda74a0bfe1c490ca8805f4be9d
mritd <mritd@linux.com>
2020-04-26 23:07:16 +0800

    chore(ci): update travis
    update travis
    
    Signed-off-by: mritd <mritd@linux.com>

---
+++ .travis.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+export TZ=UTC-8
+cd ${HOME}/manifests
+if [ ! git diff-index --quiet HEAD -- ]; then
+  git add .
+  git commit -m "Travis CI Auto Update(`date +'%Y-%m-%d %H:%M:%S'`)"
+  git push https://mritd:${GITHUB_TOKEN}@github.com/mritd/gcr.git
+fi
